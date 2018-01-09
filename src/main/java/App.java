78ce183a6a5b7cef8a72fbd729210ecd86f83f94
telpuhova <telpuhova@gmail.com>
2018-01-08 20:11:54 -0800

    html code from routes in app.java moved to handlebar templates files (views)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
 import static spark.Spark.*;
 
+//adding stuff to work with handlebars
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView; //Spark tool that allows us to pass dynamic information, like variables, from our App.java file to our template files.
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
     public static void main(String[] args) {
 
@@ -7,40 +13,49 @@ public class App {
 
         //get("/", (request, response) -> "Hello Friend!"); //<-- route
 
-        get("/", (request, response) ->
-            "<!DOCTYPE html>" +
-            "<html>" +
-            "<head>" +
-            "<title>Hello Friend!</title>" +
-            "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-            "</head>" +
-            "<body>" +
-            "<h1>Hello From Afar</h1>" +
-            "<p>Dear Friend,</p>" +
-            "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-            "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-            "<p>But I like programming a lot, so I've got that going for me. </p>" +
-            "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-            "<p>Cheers,</p>" +
-            "<p>Travel Enthusiast Jane</p>" +
-            "</body>" +
-            "</html>"
-        );
+//        get("/", (request, response) ->
+//            "<!DOCTYPE html>" +
+//            "<html>" +
+//            "<head>" +
+//            "<title>Hello Friend!</title>" +
+//            "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+//            "</head>" +
+//            "<body>" +
+//            "<h1>Hello From Afar</h1>" +
+//            "<p>Dear Friend,</p>" +
+//            "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
+//            "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
+//            "<p>But I like programming a lot, so I've got that going for me. </p>" +
+//            "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
+//            "<p>Cheers,</p>" +
+//            "<p>Travel Enthusiast Jane</p>" +
+//            "</body>" +
+//            "</html>"
+//        );
 
-        get("/favorite_photos", (request, response) ->
-            "<!DOCTYPE html>" +
-            "<html>" +
-            "<head>" +
-            "<title>Hello Friend!</title>" +
-            "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-            "</head>" +
-            "<body>" +
-            "<h1>Favorite Traveling Photos</h1>" +
-            "<ul>" +
-            "<li><img src='/images/pic.jpg' alt='A photo.'/></li>" +
-            "</ul>" +
-            "</body>" +
-            "</html>"
-        );
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
+
+//        get("/favorite_photos", (request, response) ->
+//            "<!DOCTYPE html>" +
+//            "<html>" +
+//            "<head>" +
+//            "<title>Hello Friend!</title>" +
+//            "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+//            "</head>" +
+//            "<body>" +
+//            "<h1>Favorite Traveling Photos</h1>" +
+//            "<ul>" +
+//            "<li><img src='/images/pic.jpg' alt='A photo.'/></li>" +
+//            "</ul>" +
+//            "</body>" +
+//            "</html>"
+//        );
+
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }

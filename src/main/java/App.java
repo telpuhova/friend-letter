2b9937f0b1f30ec947823ec8613a1b8892f17574
telpuhova <telpuhova@gmail.com>
2018-01-08 21:11:45 -0800

    add page with a form and a page with variables to dinamically render data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,5 +21,19 @@ public class App {
             return new ModelAndView(model, "favorite_photos.hbs");
         }, new HandlebarsTemplateEngine());
 
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
